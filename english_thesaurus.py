3208704dc513b4b03178cc3dcea62acfc8a9a075
Thatguywholearns <david.verbiest@gmail.com>
2021-04-05 13:22:44 +0200

    Add closest definition to function

---
+++ english_thesaurus.py
@@ -0,0 +1,34 @@
+import json
+from difflib import get_close_matches
+
+# Functions
+def return_definition(thesaurus_dict: dict, word: str) -> str:
+    """
+    thesaurus_dict:     Dictionary containing words and their definition
+    word:               Word you want to look up a definition for
+    returns:            Definition of the word you provided
+    """
+    # Clean input
+    word_clean = word.strip().lower()
+    if word_clean in thesaurus_dict:
+        return thesaurus_dict[word_clean]
+    elif len(get_close_matches(word=word_clean, possibilities=thesaurus_dict, cutoff=0.5)) > 0:
+        closest_word = get_close_matches(word=word_clean, possibilities=thesaurus_dict, cutoff=0.5)[0]
+        print(f"Did you mean {closest_word}")
+        user_feedback = input("[Y/N]\n")
+        if user_feedback == "Y":
+            return thesaurus_dict[closest_word]
+    else:
+        print("Couldn't find this word in the dictionary. Check if word exists")
+        return "Definition not found"
+
+
+# Start app
+with open("data.json") as file:
+    data = json.load(file)
+
+user_input = str(input("Enter a word: "))
+
+def_word = return_definition(data, user_input)
+
+print(def_word)
